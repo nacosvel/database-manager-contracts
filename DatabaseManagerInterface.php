3a78f24680dd899eeaa099880cb13d577e08743c
jundayw <jundayw@126.com>
2024-09-25 14:24:12 +0800

    pref

--- DatabaseManagerInterface.php
+++ DatabaseManagerInterface.php
@@ -4,5 +4,16 @@ namespace Nacosvel\Contracts\DatabaseManager;
 
 interface DatabaseManagerInterface
 {
+    /**
+     * @return mixed
+     */
+    public function getManager(): mixed;
+
+    /**
+     * @param mixed $manager
+     *
+     * @return static
+     */
+    public function setManager(mixed $manager): static;
 
 }
