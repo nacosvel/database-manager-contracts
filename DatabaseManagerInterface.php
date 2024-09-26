b7533c11b65c9b4eebade9e0c92dfb329b404d7e
jundayw <jundayw@126.com>
2024-09-26 19:53:01 +0800

    wip

--- DatabaseManagerInterface.php
+++ DatabaseManagerInterface.php
@@ -16,4 +16,34 @@ interface DatabaseManagerInterface
      */
     public function setManager(mixed $manager): static;
 
+    /**
+     * Start a new database transaction.
+     *
+     * @return void
+     */
+    public function beginTransaction(): void;
+
+    /**
+     * Commit the active database transaction.
+     *
+     * @return void
+     */
+    public function commit(): void;
+
+    /**
+     * Rollback the active database transaction.
+     *
+     * @return void
+     */
+    public function rollBack(): void;
+
+    /**
+     * Run a raw, unprepared query against the PDO connection.
+     *
+     * @param string $query
+     *
+     * @return void
+     */
+    public function unprepared(string $query): void;
+
 }
