274ae4f449be85135ad0245ac0f88a0d19fb5933
jundayw <jundayw@126.com>
2024-10-11 14:42:22 +0800

    wip:rename

--- DatabaseManagerInterface.php
+++ DatabaseManagerInterface.php
@@ -11,10 +11,10 @@ namespace Nacosvel\Contracts\DatabaseManager;
  * @method \void unprepared(string $query) Run a raw, unprepared query against the PDO connection.
  * @method \mixed connection(string $name = null) Get a database connection instance.
  * @method \mixed getDatabaseConfig(string $option = null) Get the configuration for a connection.
- * @method \void startTransactionXa(string $xid) Starts a distributed XA transaction with the given XID.
- * @method \void prepareXa(string $xid) Prepares the distributed XA transaction with the given XID for commit.
- * @method \void commitXa(string $xid) Commits the distributed XA transaction with the given XID.
- * @method \void rollbackXa(string $xid) Rolls back the distributed XA transaction with the given XID.
+ * @method \void xaBeginTransaction(string $xid) Starts a distributed XA transaction with the given XID.
+ * @method \void xaPrepare(string $xid) Prepares the distributed XA transaction with the given XID for commit.
+ * @method \void xaCommit(string $xid) Commits the distributed XA transaction with the given XID.
+ * @method \void xaRollBack(string $xid) Rolls back the distributed XA transaction with the given XID.
  */
 interface DatabaseManagerInterface
 {
