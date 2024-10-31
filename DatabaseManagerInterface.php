5797db1abe0c6c3aeab33fa611c7c169e103c141
jundayw <jundayw@126.com>
2024-10-31 18:17:04 +0800

    refactor

--- DatabaseManagerInterface.php
+++ DatabaseManagerInterface.php
@@ -2,20 +2,43 @@
 
 namespace Nacosvel\Contracts\DatabaseManager;
 
+use PDO;
+
 /**
  * Interface DatabaseManagerInterface
  *
- * @method \void beginTransaction() Start a new database transaction.
- * @method \void commit() Commit the active database transaction.
- * @method \void rollBack() Rollback the active database transaction.
- * @method \void unprepared(string $query) Run a raw, unprepared query against the PDO connection.
- * @method \mixed connection(string $name = null) Get a database connection instance.
- * @method \mixed getDatabaseConfig(string $option = null) Get the configuration for a connection.
- * @method \void xaBeginTransaction(string $xid) Starts a distributed XA transaction with the given XID.
- * @method \void xaPrepare(string $xid) Prepares the distributed XA transaction with the given XID for commit.
- * @method \void xaCommit(string $xid) Commits the distributed XA transaction with the given XID.
- * @method \void xaRollBack(string $xid) Rolls back the distributed XA transaction with the given XID.
+ * @method null|string getName() Get the database connection name.
+ * @method mixed getConnectionConfiguration(string $option = null, mixed $default = null) Get the configuration for a connection.
+ * @method PDO getPdo() Get the current PDO connection.
+ * @method void unprepared(string $query) Run a raw, unprepared query against the PDO connection.
+ * @method void beginTransaction() Start a new database transaction.
+ * @method void commit() Commit the active database transaction.
+ * @method void rollBack() Rollback the active database transaction.
+ * @method void xaBeginTransaction(string $xid) Starts a distributed XA transaction with the given XID.
+ * @method void xaPrepare(string $xid) Prepares the distributed XA transaction with the given XID for commit.
+ * @method void xaCommit(string $xid) Commits the distributed XA transaction with the given XID.
+ * @method void xaRollBack(string $xid) Rolls back the distributed XA transaction with the given XID.
+ * @method void xaRecover(string $xid) Recovers the distributed XA transaction with the given XID.
  */
 interface DatabaseManagerInterface
 {
+    /**
+     * Get a database connection instance.
+     *
+     * @param string|null $name
+     *
+     * @return mixed
+     */
+    public function connection(string $name = null): mixed;
+
+    /**
+     * Set the default database connection for the callback execution.
+     *
+     * @param string   $name
+     * @param callable $callback
+     *
+     * @return mixed
+     */
+    public function usingConnection(string $name, callable $callback): mixed;
+
 }
